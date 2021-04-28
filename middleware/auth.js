1e0286e84b9a7929a875bae5252eb3ca6de45670
lmontem <lmontem7@gmail.com>
2021-04-28 16:04:00 -0400

    middleware

---
+++ middleware/auth.js
@@ -0,0 +1,19 @@
+const jwt = require('jsonwebtoken');
+
+function auth(req, res, next) {
+  const { authorization } = req.headers;
+  if (!authorization || !authorization.startsWith('Bearer ')) {
+    return res.status(401).send({ message: 'Authorization required' });
+  }
+  const token = authorization.replace('Bearer ', '');
+  let payload;
+  try {
+    payload = jwt.verify(token, 'secret key');
+  } catch (err) {
+    return res.status(401).send({ message: 'Authorization required' });
+  }
+  req.user = payload;
+  next();
+}
+
+module.exports(auth);
