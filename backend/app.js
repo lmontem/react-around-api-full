2a18ec3887775ba8cad02744ebb0a4d3a89930b8
lmontem <lmontem7@gmail.com>
2021-05-10 12:10:09 -0400

    added extra security

--- backend/app.js
+++ backend/app.js
@@ -3,7 +3,8 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const { celebrate, Joi } = require('celebrate');
-
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
 const { errors } = require('celebrate');
 const cors = require('cors');
 const user = require('./routes/users.js');
@@ -28,7 +29,13 @@ mongoose.connect('mongodb://localhost:27017/aroundb', {
   useFindAndModify: false,
   useUnifiedTopology: true,
 });
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // limit each IP to 100 requests per windowMs
+});
 
+app.use(limiter);
+app.use(helmet);
 app.use(cors());
 app.use(user);
 app.use(card);
